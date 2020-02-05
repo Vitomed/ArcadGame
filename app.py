1e1b7fa3d90b8d07829c3f1fa560cfe70f19f9ab
Vitomed <viktor.medyankin@mail.ru>
2020-02-05 11:20:43 +0300

    naming for variables

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ class Board:
         self.columns = columns
         self.cells = rows * columns
 
-    def draw_board(self, board):
+    def draw_board(self, items_board):
         columns_row = ""
         print(f"{'-' * 60}")
         for i in range(0, self.cells, self.columns):
             for j in range(0, self.columns):
                 item_index = i + j
-                columns_row += f"|\t{board.get_item(item_index)}\t"
+                columns_row += f"|\t{items_board.get_item(item_index)}\t"
             columns_row += f"|\n{'-' * 60}\n"
         print(columns_row)
 
@@ -47,12 +47,15 @@ def take_input(player_token: str, items: object):
     valid = False
     while not valid:
         player_answer = input("Куда поставим " + player_token+"? ")
+
         try:
             player_answer = int(player_answer)
         except ValueError:
-            print("Некорректный ввод. Я принимаю только целые числа?")
+            print("Некорректный ввод. Я принимаю только целые числа!")
             continue
+
         if 0 <= player_answer <= 41:
+
             if str(items.get_item(player_answer))not in "XO":
                 items.set_item(player_answer, player_token)
                 valid = True
@@ -62,25 +65,36 @@ def take_input(player_token: str, items: object):
             print("Некорректный ввод. Введите число от 0 до 41 чтобы походить.")
 
 
-def coord(win_lst=None, cells=42, step=7):
-    if win_lst is not None:
-        return win_lst
+def coord(cells, step):
+    """Calculates winning coordinates
+
+    :param cells: number cells
+    :param step: step is equivalent number  of columns
+    :return: list with winning coordinates
+    """
+
     win_lst = []
 
-    win_coord_1 = [(i, i + 1, i + 2, i + 3) for n in range(0, cells-3, step) for i in range(n, n + 4)]
+    # horizontal winning values
+    win_coord_1 = [(i, i + 1, i + 2, i + 3) for n in range(0, cells, step) for i in range(n, n + 4)]
+
+    # vertical winning values
     win_coord_2 = [(i, i + 7, i + 14, i + 21) for n in range(0, 15, step) for i in range(n, n + 7)]
+
+    # victory values on the left diagonal
     win_coord_3 = [(i, i + 8, i + 16, i + 24) for n in range(0, 15, step) for i in range(n, n + 4)]
+
+    # victory values on the right diagonal
     win_coord_4 = [(i, i + 6, i + 12, i + 18) for n in range(3, 22, step) for i in range(n, n + 4)]
 
     for i in range(1, 5):
         win_lst.append((eval(f"win_coord_{i}")))
     win_lst = list(chain(*win_lst))
-
     return win_lst
 
 
-def check_win(items, win_coord):
-    board = items.cells
+def check_win(items_board: object, win_coord: list):
+    board = items_board.cells
     for each in win_coord:
         if board[each[0]] == board[each[1]] == board[each[2]] == board[each[3]]:
             if board[each[0]] == "X":
@@ -91,7 +105,7 @@ def check_win(items, win_coord):
 
 if __name__ == "__main__":
     cells = int(ROWS * COLUMNS)
-    win_answers = coord()
+    win_board = coord(cells=cells, step=COLUMNS)
     item_board = ItemsBoard(rows=ROWS, columns=COLUMNS)
     board = Board(rows=ROWS, columns=COLUMNS)
     flag = True
@@ -108,7 +122,7 @@ if __name__ == "__main__":
                 take_input("O", item_board)
             counter += 1
             if counter > 6:
-                tmp = check_win(item_board, win_coord=win_answers)
+                tmp = check_win(item_board, win_coord=win_board)
                 if tmp:
                     print(tmp)
                     break
